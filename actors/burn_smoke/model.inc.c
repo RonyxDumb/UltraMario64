0b8f53f19795286f74820c2839e7ac8b3687887e
Ronyx <104029827+RonyxDumb@users.noreply.github.com>
2023-09-01 23:05:48 +0200

    fixata la fiamma

--- actors/burn_smoke/model.inc.c
+++ actors/burn_smoke/model.inc.c
@@ -8,10 +8,7 @@ static const Vtx burn_smoke_seg4_vertex_040217C0[] = {
     {{{   -50,     50,      0}, 0, {     0,      0}, {0x14, 0x0a, 0x0a, 0xff}}},
 };
 
-// //! Wrong texture format. Called as rgba16, which makes the burn smoke appear
-//     as a transparent black burn smoke. Probably meant to show up as white-ish
-//     burn smoke, but mistakened for being intended as black smoke.
-// 0x04021800
+// PS 01/09/2023 = texture fixata nel formato .ia
 ALIGNED8 static const Texture burn_smoke_seg4_texture_04021800[] = {
 #include "actors/burn_smoke/burn_smoke.ia16.inc.c"
 };
@@ -44,7 +41,7 @@ const Gfx burn_smoke_seg4_dl_04022048[] = {
 // 0x04022070 - 0x040220C8
 const Gfx burn_smoke_seg4_dl_04022070[] = {
     gsSPDisplayList(burn_smoke_seg4_dl_04022000),
-    gsDPLoadTextureBlock(burn_smoke_seg4_texture_04021800, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 32, 0, G_TX_CLAMP, G_TX_CLAMP, 5, 5, G_TX_NOLOD, G_TX_NOLOD),
+    gsDPLoadTextureBlock(burn_smoke_seg4_texture_04021800, G_IM_FMT_IA, G_IM_SIZ_16b, 32, 32, 0, G_TX_CLAMP, G_TX_CLAMP, 5, 5, G_TX_NOLOD, G_TX_NOLOD),
     gsSPDisplayList(burn_smoke_seg4_dl_04022028),
     gsSPDisplayList(burn_smoke_seg4_dl_04022048),
     gsSPEndDisplayList(),
